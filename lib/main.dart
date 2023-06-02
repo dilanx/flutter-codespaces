27e4816e5feacc231fb30ecd4eff62b21782fe81
zavx0z <zavx0z@yahoo.com>
2023-06-02 07:37:56 +0000

    init

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Приложение которое в codespace'),
     );
   }
 }
